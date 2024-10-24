6b779f51a49eee4f0e79736fe553f426656e3a32
Fumito Ito <weathercook@gmail.com>
2024-10-25 00:54:07 +0900

    fix tests

--- Tests/SwiftyJSONLinesTests/SwiftyJSONLTests.swift
+++ Tests/SwiftyJSONLinesTests/SwiftyJSONLTests.swift
@@ -11,44 +11,46 @@ let jsonString = """
 
 @Test func fromString() throws {
     let jsonlines = try JSONLines(jsonString)
+    let dictionaries = try jsonlines.toDictionaries()!
 
-    #expect(jsonlines.lines.count == 4)
+    #expect(dictionaries.count == 4)
 
-    let gilbert = try #require(jsonlines.lines.first)
+    let gilbert = try #require(dictionaries.first)
     #expect(gilbert["name"] as? String == "Gilbert")
     #expect(gilbert["wins"] as? [[String]] == [["straight", "7♣"], ["one pair", "10♥"]])
 
-    let alexa = try #require(jsonlines.lines.dropFirst().first)
+    let alexa = try #require(dictionaries.dropFirst().first)
     #expect(alexa["name"] as? String == "Alexa")
     #expect(alexa["wins"] as? [[String]] == [["two pair", "4♠"], ["two pair", "9♠"]])
 
-    let may = try #require(jsonlines.lines.dropFirst(2).first)
+    let may = try #require(dictionaries.dropFirst(2).first)
     #expect(may["name"] as? String == "May")
     #expect(may["wins"] as? [[String]] == [])
 
-    let deloise = try #require(jsonlines.lines.dropFirst(3).first)
+    let deloise = try #require(dictionaries.dropFirst(3).first)
     #expect(deloise["name"] as? String == "Deloise")
     #expect(deloise["wins"] as? [[String]] == [["three of a kind", "5♣"]])
 }
 
 @Test func fromData() throws {
     let jsonlines = try JSONLines(data: jsonString.data(using: .utf8)!)
+    let dictionaries = try jsonlines.toDictionaries()!
 
     #expect(jsonlines.lines.count == 4)
 
-    let gilbert = try #require(jsonlines.lines.first)
+    let gilbert = try #require(dictionaries.first)
     #expect(gilbert["name"] as? String == "Gilbert")
     #expect(gilbert["wins"] as? [[String]] == [["straight", "7♣"], ["one pair", "10♥"]])
 
-    let alexa = try #require(jsonlines.lines.dropFirst().first)
+    let alexa = try #require(dictionaries.dropFirst().first)
     #expect(alexa["name"] as? String == "Alexa")
     #expect(alexa["wins"] as? [[String]] == [["two pair", "4♠"], ["two pair", "9♠"]])
 
-    let may = try #require(jsonlines.lines.dropFirst(2).first)
+    let may = try #require(dictionaries.dropFirst(2).first)
     #expect(may["name"] as? String == "May")
     #expect(may["wins"] as? [[String]] == [])
 
-    let deloise = try #require(jsonlines.lines.dropFirst(3).first)
+    let deloise = try #require(dictionaries.dropFirst(3).first)
     #expect(deloise["name"] as? String == "Deloise")
     #expect(deloise["wins"] as? [[String]] == [["three of a kind", "5♣"]])
 }
