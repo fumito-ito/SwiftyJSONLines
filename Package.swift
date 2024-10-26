fb957dada1e920b6916c7083e99895f8f7fc885a
Fumito Ito <weathercook@gmail.com>
2024-10-26 21:34:46 +0900

    fix invalid ios supporting version

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftyJSONLines",
     platforms: [
-        .iOS(.v12),
+        .iOS(.v15),
         .macOS(.v12)
     ],
     products: [
